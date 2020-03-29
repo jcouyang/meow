6632d8c28d6f1680cfe10aa03625ee7779a5f209
Jichao Ouyang <oyanglulu@gmail.com>
2020-03-29 21:39:14 +1100

    toLan.fromLan & toRan.fromLan

--- src/test/scala/KanExtensions.scala
+++ src/test/scala/KanExtensions.scala
@@ -3,12 +3,12 @@ package meow.functor
 import munit._
 import org.scalacheck.Prop._
 
-class KanExtensions extends ScalaCheckSuite {
+class KanExtensions extends FunSuite with ScalaCheckSuite {
   given Functor[Option] {
     def [A, B](r: Option[A]).map(f: A => B):Option[B] = r.map(f)
   }
 
-  property("Right Kan") {
+  property("Right Kan: fromRan . toRan") {
     forAll { (input: Option[List[Int]]) =>
       val nat: [A] => Option[List[A]] => Vector[A] = [A] => (a: Option[List[A]]) => a.map(_.toVector).getOrElse(Vector())
       val toRan: [A] => Option[A] => Ran[List, Vector, A] = [A] => (a: Option[A]) => Ran.toRan[Option, List, Vector, A](nat)(a)
@@ -18,14 +18,34 @@ class KanExtensions extends ScalaCheckSuite {
     }
   }
 
-  property("Left Kan") {
+  property("Right Kan: toRan . fromRan") {
+    forAll { (input: Option[String]) =>
+      val nat: [A] => Option[List[A]] => Vector[A] = [A] => (a: Option[List[A]]) => a.toList.flatten.toVector
+      val kran = [A] => (a: Option[A]) => Ran[List, Vector, A]([B] => (f:A => List[B])=> a.toList.flatMap(f).toVector)
+      val fromRan: [A] => Option[List[A]] => Vector[A] = [A] => (a:Option[List[A]]) => Ran.fromRan[Option, List, Vector, A](kran)(a)
+      assertEquals(Ran.toRan[Option, List, Vector, String](fromRan)(input), kran(input))
+    }
+  }
+
+  property("Left Kan: fromLan . toLan") {
     forAll { (input: Vector[Int]) =>
-      val lan: Lan[List, Vector, Int] = Lan.LeftKan((gb: List[String])=> gb.size, Vector("hehe"))
       val nat: [A] => Vector[A] => Option[List[A]] = [A] => (a: Vector[A]) => Some(a.toList)
       val toLan : [B] => Lan[List, Vector, B] => Option[B] =
         [B] => (a: Lan[List, Vector, B]) => Lan.toLan[Option, List, Vector, B](nat)(a)
 
       Lan.fromLan[Option, List, Vector, Int](toLan)(input) == nat(input)
+    }
+  }
+
+  property("Left Kan: toLan . fromLan") {
+    forAll { (input: Vector[String]) =>
+      val lan: Lan[List, Vector, Int] = Lan.LeftKan((gb: List[String])=> gb.size, input)
+      val nat: [A] => Lan[List, Vector, A] => Option[A] = [A] => (lan: Lan[List, Vector, A]) => lan match
+        case Lan.LeftKan(f, v) => Option(f(v.toList))
+      val fromLan: [A] => Vector[A] => Option[List[A]] =
+        [A] => (a:Vector[A]) => Lan.fromLan[Option, List, Vector, A](nat)(a)
+
+      Lan.toLan[Option, List, Vector, Int](fromLan)(lan) == Option(input.size)
     }
   }
 }
