cd0f6ca18dec43d4dc51954f793c1ec9f0828b35
Jichao Ouyang <oyanglulu@gmail.com>
2021-02-28 13:22:12 +1100

    fix functor docs format

--- src/main/scala/Functor.scala
+++ src/main/scala/Functor.scala
@@ -7,63 +7,68 @@ import Function._
 
 case class Identity[A](run: A)
 /**
-  A type `F` is a Functor if it provides a function `fmap` which, given any types `A` and `B`
-  lets you apply any function from ~A => B~ to turn an `F[A]` into an `F[B]`, preserving the
-  structure of `F`. Furthermore `F` needs to adhere to the following:
-
-  ### Identity
-  ```scala
-  fa <#> identity == identity(fa)
-  ```
-  ### Composition
-  ```
-  (f compose g) `<$>` fa  == (map[F](f) compose map[F](g))(fa)
-  ```
+  * A type `F` is a Functor if it provides a function `fmap` which, given any types `A` and `B`
+  * lets you apply any function from ~A => B~ to turn an `F[A]` into an `F[B]`, preserving the
+  * structure of `F`. Furthermore `F` needs to adhere to the following:
+  *
+  * ### Identity
+  * ```scala
+  * fa <#> identity == identity(fa)
+  * ```
+  * ### Composition
+  * ```scala
+  * (f compose g) `<$>` fa  == (map[F](f) compose map[F](g))(fa)
+  * ```
   */
 trait Functor[F[_]]:
   def fmap[A, B](f: A => B): F[A] => F[B]
   extension [A, B](fa: F[A])
     infix def map(f: A => B): F[B] = fmap(f)(fa)
 
-    /**
-      Usually [[this.fmap]] takes function first, `mapFlipped` takes data first
-      ```scala
-      fa <#> identity == identity(fa)
-      ```
+    /** Usually [[this.fmap]] takes function first, `mapFlipped` takes data first
+      * ```scala
+      * fa <#> identity == identity(fa)
+      * ```
       */
     @targetName("mapFlipped")
     def <#>(f: A => B): F[B] = fmap(f)(fa)
 
-    /**
-      drop whatever value `A` on left and return `F[B]` on the right
-      ```scala
-      Option(1) `$>` 3 == Option(3)
-      ```
+    /** Drop whatever value `A` on left and return `F[B]` on the right
+      * ```scala
+      * Option(1) `$>` 3 == Option(3)
+      * ```
       */
     @targetName("voidLeft")
     infix def `$>`(a: B): F[B] = fmap(const[B, A](a))(fa)
 
-    /**
-      Void `F[A]` and return `F[Unit]`
-      ```scala
-      Option(2).void == Option(())
-      ```
+    /** Void `F[A]` and return `F[Unit]`
+      * ```scala
+      * Option(2).void == Option(())
+      * ```
       */
     def void: F[Unit] = fmap(const[Unit, A](()))(fa)
 
 end Functor
 
-/**
-  Instances and static functions for [[meow.data.Functor]]
-  */
+/** Instances and static functions for [[meow.data.Functor]] */
 object Functor:
   def map[F[_]](using Functor[F]) = [A, B] => (f: A => B) => (fa: F[A]) => fa.map(f)
 
   extension [F[_], A, B](f: A => B)
+    /** Infix syntax for [[meow.data.Functor#fmap]]
+      * ```scala
+      * (f compose g) `<$>` fa  == (map[Option](f) compose map[Option](g))(fa)
+      * ```
+      */
     @targetName("fmap")
     def `<$>`(fa: F[A])(using Functor[F]): F[B] = fa map f
 
   extension [F[_], A, B](a: A)
+    /** Void right
+      * ```scala
+      * 2 `<$` Option(3) == Option(2)
+      * ```
+      */
     @targetName("voidRight")
     def `<$`(fb: F[B])(using Functor[F]): F[A] = fb.map(const(a))
 
